643ca3f8192047ab01160e9e29935bc2380c2c6e
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-07-13 13:12:42 +0500

    js add

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -1,9 +1,11 @@
+ let elSubmitBtn = document.querySelector('#submit-btn');
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-
+  
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
@@ -15,7 +17,24 @@
     appId: "1:626955745254:web:58a08d4093d044fe520827",
     measurementId: "G-YPCLRRMX7V"
   };
-
+  
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+  
+  elSubmitBtn.addEventListener('click', () => {
+    let elEmail = document.querySelector('#email').value;
+    let elPassword = document.querySelector('#password').value;
+  
+    createUserWithEmailAndPassword(auth, elEmail, elPassword)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        window.location.href = 'index.html';
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  });
